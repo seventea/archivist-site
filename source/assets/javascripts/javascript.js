65a755ae5f93af24fc6a479061e104362cc8ab6b
Eric Cohen <eirc.eirc@gmail.com>
2015-01-04 14:59:35 +0200

    Change OpenTips style a bit and factor it out.

--- source/assets/javascripts/javascript.js
+++ source/assets/javascripts/javascript.js
@@ -6,6 +6,14 @@
  * Copyright (c) 2014, Seventea Studios
  */
 (function (window, document, undefined) {
+    Opentip.styles.social = {
+      extends: 'dark',
+      target: true,
+      targetJoint: 'bottom',
+      tipJoint: 'top'
+    };
+    Opentip.defaultStyle = 'social';
+
     window.onload = function () {
         window.Archivist = {};
 
